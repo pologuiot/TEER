01310769151cadbd7d5243125ac15f6cebf7e095
clarissektz <161049218+clarissektz@users.noreply.github.com>
2024-04-08 22:22:06 +0200

    function.h

--- function.h
+++ function.h
@@ -11,7 +11,7 @@ protected :
     // Fonction f
     Eigen::VectorXd _f;
     // Constantes
-    double _D, _mu_fluide, _m_part, _rho_fluide, _g, _const_G, _h ;
+    double _D, _R_vaisseau, _mu_fluide, _m_part, _rho_fluide, _g, _const_G, _h, _MomentCouette;
     // Écriture du fichier
     std::ofstream _file_out;
 
@@ -26,26 +26,33 @@ public :
     // Initialiser le nom du fichier solution
     void InitializeFileName(const std::string file_name);
     
-    // Pour récupérer _f
-    const Eigen::VectorXd &  GetF()  const;
-    
     // Sauvegarde la solution
     void SaveSolution(const double t, const Eigen::VectorXd & position, const Eigen::VectorXd & vitesse);
 
     // Initialise les paramêtres
-    void Initialize(double D, double mu, double m_particule, double rho_fluide);
+    void Initialize(double D, double mu, double m_particule, double rho_fluide, double R_vaisseau);
 
     // Calcul de la vitesse du fluide
-    double fluidspeed(double y, double t); // vitesse du juste selon x
+    double FluidSpeed(double y, double t); // vitesse du juste selon x
+    
+    // Dérivée de la vitesse du fluide à t fixé
+    double FluidSpeedDerive(double y, double t); // juste selon x
 
     // Construction de la fonction f pour résoudre u' = f
     void BuildF(double y, double t, const Eigen::VectorXd & vitesse);
     
-    // Force de trainée
-    double Force_trainée(double rho, double S, double u, double Cd);
+    // Pour récupérer _f
+    const Eigen::VectorXd & GetF() const;
+    
+    // Calcule la vitesse de cisaillement transversal
+    double VitesseCouette(double y, double Shear); //selon x
 
+    // calcule le moment (vitesse angulaire) obtenu avec couette
+    double MomentCouette(double y, double Shear); //selon z
+    
+    
     //Force de magnus
-    double Force_magnus(double rho, double S, double u, double rot,double Cm);
+    //double Force_magnus(double rho, double S, double u, double rot,double Cm);
 
 };
 
