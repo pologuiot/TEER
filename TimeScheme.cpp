75d35eab72f9ae97cd094159b8ccaeb295ce1434
clarissektz <161049218+clarissektz@users.noreply.github.com>
2024-04-08 22:23:39 +0200

    TimeScheme.cpp

--- TimeScheme.cpp
+++ TimeScheme.cpp
@@ -13,24 +13,26 @@ using namespace Eigen;
 // -------- CONSTRUCTEUR --------
 
 // Constructeur par défaut (ne pas oublier de mettre votre pointeur à 0 !!)
-EulerScheme::EulerScheme() : _sys(0)
+TimeScheme::TimeScheme() : _sys(0)
 {
 }
 
 // Destructeur
-EulerScheme::~EulerScheme()
+TimeScheme::~TimeScheme()
 {
 }
 
 // -------- INITIALISATION DES VARIABLES --------
 
-void EulerScheme::Initialize(double t0, double dt, Eigen::VectorXd & vitesse_init, Eigen::VectorXd & position_init, std::string name_file, Particule* sys)
+void TimeScheme::Initialize(double t0, double dt, Eigen::VectorXd & vitesse_init, Eigen::VectorXd & position_init, std::string name_file, Particule* sys)
 {
     this->_dt = dt;
     this->_t = t0 ;
     this->_vitesse_init = vitesse_init;
     this->_position_init = position_init;
-    this->_vitesse = vitesse_init;
+    this->_v_poseuille = vitesse_init;
+    //this->_v_couette = ?; //jsp c'est quoi les CL a t=0
+    //this->_moment = ?;
     this->_position = position_init;
     this->_sys = sys;
     if (name_file.size() > 0)
@@ -43,27 +45,50 @@ void EulerScheme::Initialize(double t0, double dt, Eigen::VectorXd & vitesse_ini
 
 
 // Enregistre la solution : fait appel à OdeSystem car la solution
-// que l'on souhaite sauvegarder peut être différente de _vitesse SaveSolution
+// que l'on souhaite sauvegarder peut être différente de _v_poseuille SaveSolution
 // le système
-void EulerScheme::SaveSolution()
+void TimeScheme::SaveSolution()
 {
-   this->_sys->SaveSolution(this->_t, this->_position, this->_vitesse);
+   this->_sys->SaveSolution(this->_t, this->_position, this->_v_poseuille);
 }
 
 
 // Fonction pour avancer en temps avec la méthode d'Euler explicite
-void EulerScheme::AdvanceVitesse()
+void TimeScheme::AdvanceVitessePoiseuille()
 {
-    this->_sys->BuildF(this->_position(1), this->_t, this->_vitesse);
-    this->_vitesse += this->_dt*this->_sys->GetF();
+    this->_sys->BuildF(this->_position(1), this->_t, this->_v_poseuille);
+    this->_v_poseuille += this->_dt*this->_sys->GetF();
     this->_t += this->_dt;
 }
 
 
-// Mise à jour de la position de la particule
-void EulerScheme::AdvancePosition()
+// Calcul du coefficient S (shear)
+void TimeScheme::AdvanceShear()
 {
-    this->_position += this->_vitesse * this->_dt ;
+    this->_Shear = this->_sys->FluidSpeedDerive(this->_position(1), this->_t) ;
+    // Il faut calculer l'expression de la vitesse dérivée selon y, a t fixé
+}
+
+
+// Fonction pour avancer en temps la vitesse de couette (cisaillement)
+void TimeScheme::AdvanceVitesseCouette()
+{
+    this->_v_couette(0) = this->_sys->VitesseCouette(this->_position(1), this->_Shear);
+    this->_v_couette(1) = 0;
+}
+
+// Avance en temps le moment (cisaillement)
+void TimeScheme::AdvanceMomentCouette()
+{
+    this->_moment(2) = this->_sys->MomentCouette(this->_position(1), this->_Shear);
+    this->_moment(0) = 0;
+    this->_moment(1) = 0;
+}
+
+// Mise à jour de la position de la particule
+void TimeScheme::AdvancePosition()
+{
+    this->_position += (this->_v_poseuille + this->_v_couette) * this->_dt ;
 }
 
 
