41c4bed6cce043727e4c9812a919582b21ff426b
pologuiot <160762643+pologuiot@users.noreply.github.com>
2024-04-08 17:13:34 +0200

    Add files via upload

--- function.cpp
+++ function.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <complex>
 
+
 using namespace std;
 using namespace Eigen;
 
@@ -41,6 +42,7 @@ void Particule::Initialize(double D, double mu_fluide, double m_part, double rho
     this->_g = 9.81 ;
     this->_const_G = 50.33 ;
     this->_h = 400e-6 ; // distance entre les 2 parois
+    this->_f.resize(2) ;
 }
 
 
@@ -74,33 +76,43 @@ double Particule::fluidspeed(double y, double t)
     double nu=7.31e-7;
     double omega=12;
     double xi = 0.27;
-    complex<double> i(0, 1) ;
-    complex<double> omega0= i*omega/nu;
-    complex<double> u = (this->_const_G / (2.0 * this->_rho_fluide *nu) ) * (this->_h * this->_h / 4.0 - y * y) + (this->_const_G * xi / (i * this->_rho_fluide * omega )) * (1.0 - cosh(omega0 * y) / cosh(omega0 * (this->_h / 2))) * exp(i * omega * t);
-    return u.real();
+    double G=50.33;
+    double rho=1000;
+    double h=400e-6; 
+    double alpha = sqrt(omega/(2*nu));
+
+    double aa = (exp(alpha*y)+exp(-alpha*y))*cos(alpha*y);
+    double bb = (exp(alpha*y)-exp(-alpha*y))*sin(alpha*y);
+    double cc = (exp(alpha*h/2.0)+exp(-alpha*h/2.0))*cos(alpha*h/2.0);
+    double dd = (exp(alpha*h/2.0)-exp(-alpha*h/2.0))*sin(alpha*h/2.0);
+
+    // Expression calculé à la main 
+    double u = (1/(rho*omega))*(-G*xi*sin(omega*t)*(1-(aa*cc+bb*dd)/(cc*cc+dd*dd))+G*xi*cos(omega*t)*((aa*dd-bb*cc)/(cc*cc-dd*dd))) + ((G*h*h)*(1-(2*y/h)*(2*y/h)))/(8*rho*nu) ;
+
+    return u;
 }
 
 
 // Construction de la fonction f pour résoudre u' = f
 void Particule::BuildF(double y, double t, const VectorXd & vitesse)
 {
-    _f = vitesse ;
     _f(0) = ((3 * this->_mu_fluide * EIGEN_PI * this->_D)/this->_m_part) * (fluidspeed(y, t)- vitesse(0)) ;
-    _f(1) = (this->_g * (this->_rho_fluide * (4/3 * EIGEN_PI * pow(this->_D/2, 3)) - this->_m_part) -3 * this->_mu_fluide * EIGEN_PI * this->_D * vitesse(1)) / this->_m_part ;
+    _f(1) = (this->_g * (this->_rho_fluide * (4./3. * EIGEN_PI * pow(this->_D/2, 3)) - this->_m_part) - (3.0 * this->_mu_fluide * EIGEN_PI * this->_D * vitesse(1))) / this->_m_part ; 
+
+    // cout << " f(0) = " << _f(0) << endl ;
+    // cout << " f(1) = " << _f(1) << endl ;
 }
 
 // Calcule la force de trainée
 double Particule :: Force_trainée(double rho, double S, double u, double Cd)
 {
     return 0;
-
 }
 
 // Calcule la force de Magnus
 double Particule :: Force_magnus(double rho, double S, double u, double rot,double Cm)
 {
     return 0;
-    
 }
 
 #define _FUNCTION_CPP
