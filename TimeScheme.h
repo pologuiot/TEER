0eeeff1b055899dfc4de00d76befeb799896b746
clarissektz <161049218+clarissektz@users.noreply.github.com>
2024-04-08 22:23:05 +0200

    TimeScheme.h

--- TimeScheme.h
+++ TimeScheme.h
@@ -6,7 +6,7 @@
 #include <complex>
 
 
-class EulerScheme {
+class TimeScheme {
 
 private:
     
@@ -19,8 +19,11 @@ protected:
     // Temps initial et Temps en cours et pas de Temps
     double _t, _dt;
     
-    // Vecteur solution
-    Eigen::VectorXd _vitesse, _position;
+    // Vecteurs solutions
+    Eigen::VectorXd _v_poseuille, _v_couette, _moment, _position;
+    
+    //coefficient shear
+    double _Shear;
     
     // Pointeur vers le système d'EDO
     Particule* _sys;
@@ -28,10 +31,10 @@ protected:
     
 public:
     // Constructeur
-    EulerScheme();
+    TimeScheme();
     
     // Destructeur
-    ~EulerScheme();
+    ~TimeScheme();
 
     // Initialisation deS différentes variables
     void Initialize(double t0, double dt, Eigen::VectorXd & vitesse_init, Eigen::VectorXd & position_init, std::string name_file, Particule* sys);
@@ -39,11 +42,20 @@ public:
     // Enregistre la solution un fichier
     void SaveSolution();
     
-    // Fonction pour avancer en temps avec la méthode d'Euler explicite
-    void AdvanceVitesse();
+    // Fonction pour avancer en temps la vitesse de la particule pesante avec Euler explicite
+    void AdvanceVitessePoiseuille();
+    
+    // Fonction pour avancer en temps la vitesse de couette (cisaillement)
+    void AdvanceVitesseCouette();
+    
+    // Mise à jour du moment
+    void AdvanceMomentCouette();
     
     // Mise à jour de la position de la particule
     void AdvancePosition();
+    
+    // Calcul du coefficient S (shear)
+    void AdvanceShear();
 
 };
 
